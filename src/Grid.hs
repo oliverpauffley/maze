dd6ef9e8170d0c2d74d6aa0a285fac56f87182ea
oliverpauffley <mrpauffley@gmail.com>
2024-07-12 13:11:54 +0100

    feat: get cells

--- src/Grid.hs
+++ src/Grid.hs
@@ -15,17 +15,33 @@ mkGrid rows = do
     guard (all (\row -> NE.length row == n) rows) -- Ensure all rows have the same length
     return (Grid rows)
 
+getDims :: Grid a -> (Int, Int)
+getDims (Grid rows)= do
+  let x = NE.length (NE.head rows)
+  let y = NE.length rows
+  (x, y)
+
+getCell :: Grid a -> (Int, Int) -> Maybe a
+getCell g@(Grid rows) (a, b)
+  | a >= fst (getDims g) = Nothing
+  | b >= snd (getDims g) = Nothing
+  | a < 0  = Nothing
+  | b < 0 = Nothing
+  | otherwise = Just $ (rows NE.!! a) NE.!! b
+
 -- Convert Grid to Box for pretty printing
 gridToBox :: Show a => Grid a -> B.Box
-gridToBox (Grid rows) = B.vsep 1 B.left (map rowToBox (NE.toList rows))
+gridToBox (Grid rows) = B.vsep 1 B.left (fmap rowToBox rows)
   where
     rowToBox :: Show a => NE.NonEmpty a -> B.Box
-    rowToBox = B.hsep 2 B.top . map (B.text . show) . NE.toList
+    rowToBox = B.hsep 2 B.top . fmap (B.text . show)
 
 -- Pretty print Grid
-prettyPrintGrid :: Show a => Maybe (Grid a) -> String
-prettyPrintGrid (Just grid) = (B.render . gridToBox) grid
-prettyPrintGrid Nothing     = ""
+prettyPrintGrid :: Show a => Grid a -> String
+prettyPrintGrid = B.render . gridToBox
 
-exampleGrid :: Maybe (Grid Integer)
-exampleGrid = mkGrid ((1 NE.:| [2, 3]) NE.:| [(4 NE.:| [5, 6]), (7 NE.:| [8, 9])])
+exampleGrid :: Grid Integer
+exampleGrid =
+  let (Just rows ) = mkGrid ((1 NE.:| [2, 3]) NE.:| [(4 NE.:| [5, 6]), (7 NE.:| [8, 9])])
+  in
+      rows
