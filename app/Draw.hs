ad5c1b561fce570a91632b4fbe554114a2cf8085
oliverpauffley <mrpauffley@gmail.com>
2025-05-25 17:51:29 +0100

    feat: wip diagrams over gloss

--- app/Draw.hs
+++ app/Draw.hs
@@ -1,104 +1,96 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE RecordWildCards           #-}
+{-# LANGUAGE TypeFamilies              #-}
 
 module Draw where
 
-import           App               (Config (..), MazeBuilder)
-import           Control.Monad.RWS (MonadReader (ask), MonadState (get), asks)
-import qualified Data.Map          as Map
-import qualified Graphics.Gloss    as Gloss
-import           Maze              (Edge (Edge), Edges, Maze, Node (Node),
-                                    NodeID (NodeID), Path (Closed, Open),
-                                    mazeToList, value)
+import           App                  (Config (..), MazeBuilder)
+import           Control.Monad.RWS    (MonadReader (ask), MonadState (get),
+                                       asks)
+import qualified Data.Map             as Map
+import           Diagrams.Backend.SVG
+import           Diagrams.Prelude
+import           Maze                 (Edge (Edge), Edges, Maze, Node (Node),
+                                       NodeID (NodeID), Path (Closed, Open),
+                                       mazeToList, nid, value)
 
-drawMaze :: [NodeID] -> [NodeID] -> MazeBuilder Config Maze Gloss.Picture
+drawMaze :: [NodeID] -> [NodeID] -> MazeBuilder Config Maze (Diagram B)
 drawMaze solution deadEnds = do
   Config {..} <- ask
   maze <- get
   mazePicture <- mapM drawNode (mazeToList maze)
-  solutionPicture <- drawSolution solution
-  deadEndCount <- drawDeadEnds deadEnds
-  return $ Gloss.pictures (solutionPicture : deadEndCount : mazePicture)
+  -- solutionPath <- drawSolution solution
+  -- deadEndCount <- drawDeadEnds deadEnds
+  -- return $ vsep 10 [solutionPath ||| mazePicture, deadEndCount]
+  return $ position mazePicture
+  where
+    nodePos = p2 . (\(NodeID (x, y)) -> (x, y)) . nid
 
 maxSolutionInMaze :: [NodeID] -> Maze -> Maybe Int
 maxSolutionInMaze xs m = do
   square <- Map.lookup (head xs) m
-  value square
+  Maze.value square
 
-drawNode :: Node (Maybe Int) Maze.Path -> MazeBuilder Config Maze Gloss.Picture
 drawNode (Node (NodeID (x, y)) val n s e w) = do
-  Config {..} <- ask
   debugLabels <- labels val
-  colors <- colorNode val
+  -- colors <- colorNode val
   edges <- drawEdges (n, s, e, w)
-  return $
-    Gloss.Pictures $
-      map (translation lineLength x y) (debugLabels : colors : edges)
-  where
-    translation ll a b = Gloss.translate (fromIntegral a * ll) (fromIntegral b * ll)
+  return (fromIntegral x ^& fromIntegral y, debugLabels `atop` edges)
 
-labels :: (Show a) => Maybe a -> MazeBuilder Config m Gloss.Picture
+labels :: (Show a) => Maybe a -> MazeBuilder Config m (Diagram B)
 labels Nothing = return mempty
 labels (Just a) = do
-  Config {..} <- ask
+  debug <- asks debug
   if not debug
     then return mempty
     else
-      return $
-        Gloss.translate (lineLength / 2) (lineLength / 2) $
-          Gloss.scale 0.1 0.1 $
-            Gloss.text $
-              show a
+      return $ scale 0.5 (text (show a))
 
-drawSolution :: [NodeID] -> MazeBuilder Config m Gloss.Picture
+drawSolution :: [NodeID] -> MazeBuilder Config m (Diagram B)
 drawSolution solution = do
   Config {..} <- ask
   if solve
-    then return $ Gloss.Color Gloss.red $ Gloss.line $ map (pos lineLength . toPath) solution
+    then return $ fromVertices $ map toPoint solution
     else return mempty
   where
-    pos ll (x, y) = (x * ll + ll / 2, y * ll + ll / 2)
-    toPath (NodeID (x, y)) = (fromIntegral x, fromIntegral y)
+    toPoint (NodeID (x, y)) = p2 (fromIntegral x, fromIntegral y)
 
-drawEdges :: Edges Maze.Path -> MazeBuilder Config m [Gloss.Picture]
+drawEdges :: Edges Maze.Path -> MazeBuilder Config m (Diagram B)
 drawEdges (n, s, e, w) = do
   ll <- asks lineLength
-  return
-    [ drawEdge n [(0, ll), (ll, ll)],
-      drawEdge s [(0, 0), (ll, 0)],
-      drawEdge e [(ll, 0), (ll, ll)],
-      drawEdge w [(0, 0), (0, ll)]
-    ]
+  return $
+    mconcat
+      [ drawEdge n (map p2 [(0, ll), (ll, ll)]),
+        drawEdge s (map p2 [(0, 0), (ll, 0)]),
+        drawEdge e (map p2 [(ll, 0), (ll, ll)]),
+        drawEdge w (map p2 [(0, 0), (0, ll)])
+      ]
+      # lwO 10
 
-colorNode :: Maybe Int -> MazeBuilder Config m Gloss.Picture
+drawEdge :: (TrailLike t, Monoid t) => Maybe (Edge e) -> [Point (V t) (N t)] -> t
+drawEdge Nothing p = fromVertices p
+drawEdge (Just (Edge _ e)) p = case e of
+  Open   -> mempty
+  Closed -> fromVertices p
+
+colorNode :: Maybe Int -> MazeBuilder Config m (Diagram B)
 colorNode ma = do
   Config {..} <- ask
   if not withColor
     then return mempty
-    else return $ trans lineLength $ colorN lineLength ma mazeSize
+    else return $ colorN lineLength ma mazeSize
   where
-    trans ll = Gloss.translate (ll / 2) (ll / 2)
+    colorN :: Double -> Maybe Int -> Int -> Diagram B
+    colorN ll Nothing _ = square ll # fc black
+    colorN ll (Just a) mS = square ll # fcA (colorMix (fromIntegral mS) (fromIntegral a))
 
-    colorN ll Nothing _   = Gloss.color Gloss.black $ rec ll
-    colorN ll (Just a) mS = colorScale mS a $ rec ll
+    colorMix m val = greenyellow `withOpacity` colorScale m val
+    colorScale m val = val - m / m
 
-    rec ll = Gloss.rectangleSolid ll ll
-
-    colorScale m val = Gloss.color $ Gloss.mixColors (fromIntegral m * 4) (fromIntegral $ val - m) greenSmoke Gloss.black
-
-greenSmoke :: Gloss.Color
-greenSmoke = Gloss.makeColorI 113 123 112 1
-
-drawEdge :: Maybe (Maze.Edge Maze.Path) -> Gloss.Path -> Gloss.Picture
-drawEdge Nothing p = Gloss.Line p
-drawEdge (Just (Edge _ e)) path = case e of
-  Open   -> Gloss.Blank
-  Closed -> Gloss.Line path
-
-drawDeadEnds :: [NodeID] -> MazeBuilder Config s Gloss.Picture
+drawDeadEnds :: [NodeID] -> MazeBuilder Config s (Diagram B)
 drawDeadEnds ns = do
   Config {..} <- ask
   if countDeadEnds
-    then return (Gloss.scale 0.1 0.1 (Gloss.translate 0 (-120) (Gloss.text ("Dead ends: " <> show (length ns)))))
-    else return Gloss.blank
-
--- TODO look into simulate from Gloss to show generation steps
+    then return (text ("Dead ends: " <> show (length ns)))
+    else return mempty
--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,8 @@
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedRecordDot       #-}
+{-# LANGUAGE RecordWildCards           #-}
+{-# LANGUAGE TypeFamilies              #-}
 
 module Main where
 
@@ -11,46 +14,93 @@ import qualified Algorithm.Sidewinder         as Sidewinder
 import qualified Algorithm.Wilson             as Wilson
 import           App
 import           DeadEnds                     (getDeadEnds)
+import           Diagrams.Backend.SVG.CmdLine
+import           Diagrams.Prelude             hiding (connect)
 import           Draw                         (drawMaze)
-import           Graphics.Gloss
 import           Image                        (maskToBlankMaze)
-import           Maze                         (Maze, NodeID, newMaze)
+import           Maze                         (Edge (..), Edges, Maze,
+                                               NodeID (..), Path (..), connect,
+                                               newMaze)
 import           Options.Applicative
 import           Param
 import           Solve                        (findLongestRoute)
 
+mycircle :: Diagram B
+mycircle = circle 1
+
+myLine :: Diagram B
+myLine = (fromVertices $ map p2 [(0, 0), (100, 0), (100, 100), (0, 100)]) # lwO 20
+
+myLine1 :: Diagram B
+myLine1 = fromVertices $ map p2 [(0, 0), (100, 0)]
+
+myLine2 :: Diagram B
+myLine2 = fromVertices $ map p2 [(100, 0), (100, 100)]
+
+drawEdges :: Edges Maze.Path -> Diagram B
+drawEdges (n, s, e, w) =
+  mconcat
+    [ drawEdge n (map p2 [(0, 100), (100, 100)]),
+      drawEdge s (map p2 [(0, 0), (100, 0)]),
+      drawEdge e (map p2 [(100, 0), (100, 100)]),
+      drawEdge w (map p2 [(0, 0), (0, 100)])
+    ]
+    # lwO 10
+
+drawEdge :: (TrailLike t, Monoid t) => Maybe (Edge e) -> [Point (V t) (N t)] -> t
+drawEdge Nothing p = fromVertices p
+drawEdge (Just (Edge _ e)) p = case e of
+  Open   -> mempty
+  Closed -> fromVertices p
+
+joined = (myLine1 <> myLine2) # lwO 15
+
+nID = NodeID (0, 0)
+
+exampleEdges = (Just (Edge nID Open), Just (Edge nID Closed), Just (Edge nID Closed), Just (Edge nID Closed))
+
+exampleMaze = connect (NodeID (0, 0)) (NodeID (1, 0)) (newMaze 2)
+
 main :: IO ()
 main = do
-  customExecParser p opts >>= run
+  (diagram, _) <- m
+  mainWith $ diagram
   where
-    opts =
-      info
-        (algorithm <**> helper)
-        (fullDesc <> progDesc "Create and solve random mazes!")
-    p = prefs showHelpOnEmpty
+    c = Config 1 30 False False False False Nothing
+    m = runBuilder (BinaryTree.generateMaze >> Solve.findLongestRoute >> drawMaze [] []) c (newMaze c.mazeSize)
 
-run :: Algorithm -> IO ()
-run alg = do
-  case alg of
-    BinaryTree cfg -> do
-      runAlgorithm BinaryTree.generateMaze cfg
-    Sidewinder cfg -> do
-      runAlgorithm Sidewinder.generateMaze cfg
-    AldousBroder cfg -> do
-      runAlgorithm AldousBroder.generateMaze cfg
-    Wilson cfg -> do
-      runAlgorithm Wilson.generateMaze cfg
-    HuntKill cfg -> do
-      runAlgorithm HuntKill.generateMaze cfg
-    RecursiveBacktrack cfg -> do
-      runAlgorithm RecursiveBacktrack.generateMaze cfg
-  pure ()
-  where
-    runAlgorithm :: MazeBuilder Config Maze () -> Config -> IO ()
-    runAlgorithm generate c = do
-      let m = newMaze c.mazeSize
-      (solution, maze) <- runBuilder (maskToBlankMaze >> generate >> Solve.findLongestRoute) c m
-      (deadEnds, maze') <- runBuilder getDeadEnds c maze
-      (picture, _) <- runBuilder (drawMaze solution deadEnds) c maze'
-      display (InWindow "Maze" (100, 100) (startWindowPos c.lineLength c.mazeSize)) white picture
+-- main :: IO ()
+-- main = do
+--   customExecParser p opts >>= run
+--   where
+--     opts =
+--       info
+--         (algorithm <**> helper)
+--         (fullDesc <> progDesc "Create and solve random mazes!")
+--     p = prefs showHelpOnEmpty
 
+-- run :: Algorithm -> IO ()
+-- run alg = do
+--   case alg of
+--     BinaryTree cfg -> do
+--       runAlgorithm BinaryTree.generateMaze cfg
+--     Sidewinder cfg -> do
+--       runAlgorithm Sidewinder.generateMaze cfg
+--     AldousBroder cfg -> do
+--       runAlgorithm AldousBroder.generateMaze cfg
+--     Wilson cfg -> do
+--       runAlgorithm Wilson.generateMaze cfg
+--     HuntKill cfg -> do
+--       runAlgorithm HuntKill.generateMaze cfg
+--     RecursiveBacktrack cfg -> do
+--       runAlgorithm RecursiveBacktrack.generateMaze cfg
+--   pure ()
+--   where
+--     runAlgorithm :: MazeBuilder Config Maze () -> Config -> IO ()
+--     runAlgorithm generate c = do
+--       let m = newMaze c.mazeSize
+--       (solution, maze) <- runBuilder (maskToBlankMaze >> generate >> Solve.findLongestRoute) c m
+--       (deadEnds, maze') <- runBuilder getDeadEnds c maze
+--       (picture, _) <- runBuilder (drawMaze solution deadEnds) c maze'
+
+-- display (InWindow "Maze" (100, 100) (startWindowPos c.lineLength c.mazeSize)) white picture
